294ed204a8f7bbdab747aebbac5c7954780e2fcc
altai861 <altaizandanbileg@gmail.com>
2024-05-22 08:25:36 +0800

    changed origin

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,7 +1,7 @@
 const allowedOrigins = [
     'http://localhost:5173',
     'http://localhost:4173',
-    'https://altai861.github.io/bank-frontend/',
+    'https://altai861.github.io',
 ]
 
 module.exports = allowedOrigins
